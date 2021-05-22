0f0b4122f5d9c6f47944c185ae014ca6c90b2770
jwm00 <47438768+JumiDeluxe@users.noreply.github.com>
2021-05-23 00:08:45 +0200

    NVIDIA support, restore option, update manual
    - Added support for NVIDIA as using it requires to use HEAD-number for xsetwacom's MapToOutput argument instead of just monitor name from xrandr.
    - Restore option enables disabling our wacom-to-monitor mappings (basically maps it to the default resolution)
    - Updated help to mention restore option
    - Small changes in CLI action signalisation ("Mappting..." instead of "Mapped" during script execution, etc.)

--- wacom_mapper.sh
+++ wacom_mapper.sh
@@ -1,38 +1,61 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+help () {
+  echo "Usage: $0 [option]"
+  echo
+  echo "Options:"
+  echo "neutral number to select monitor"
+  echo "-r to restore your default mapping"
+  echo "leave empty to select the first screen"
+  echo
+  echo "Monitor is choosed depending on position in xrandr listing starting from 0"
+}
+
+VALID_ARGUMENT_RE='^[0-9]+$'
+DEFAULT_SCREEN_RES=$(xrandr | head -n 1 | sed 's/.*current //' | sed 's/, max.*//' | sed 's/ x /x/')+0+0
+RESTORE=false
+
+if [ -z $1 ]
   then
   MONITOR_ID=0
-elif [ "$1" == "--help" ]
+elif [ $1 == "-r" ]
   then
-    echo "Usage: $0 [monitor]"
-    echo
-    echo "Monitor is choosed depending on position in xrandr listing starting from 0"
-  exit 0
+  RESTORE=true  
+elif ! [[ $1 =~ $VALID_ARGUMENT_RE ]]
+  then
+    help
+    exit 0
 else
   MONITOR_ID=$1
+fi
 
+if $RESTORE
+  then
+  MONITOR_NAME=$DEFAULT_SCREEN_RES
+else
   NUMBER_OF_CONNECTED_MONITORS=$(xrandr --listactivemonitors | grep "Monitors:" | cut -d " " -f 2)
-  MAX_MONITOR_ID=${NUMBER_OF_CONNECTED_MONITORS+1}
+  MAX_MONITOR_ID=$((${NUMBER_OF_CONNECTED_MONITORS}-1))
 
   if [ $MONITOR_ID -gt $MAX_MONITOR_ID ]
     then
     MONITOR_ID=$MAX_MONITOR_ID
-  elif [ $MONITOR_ID -lt 0 ]
-    then
-    MONITOR_ID=0
   fi
   
-fi
+  OPENGL_GRAPHICS=$(glxinfo|egrep "OpenGL vendor|OpenGL renderer")
 
-MONITOR_NAME=$(xrandr --listactivemonitors | grep "$MONITOR_ID:" | tr -s " " | cut -d " " -f 5)
+  MONITOR_NAME=$(xrandr --listactivemonitors | grep "$MONITOR_ID:" | tr -s " " | cut -d " " -f 5)
+
+  if [[ "$OPENGL_GRAPHICS" == *"NVIDIA"* ]]; then
+    MONITOR_NAME="HEAD-"$MONITOR_ID
+  fi
+fi
 
 xsetwacom list devices |
 while IFS= read -r line; do
   WACOM_DEVICE="$(echo $line)"
   WACOM_DEVICE_ID="$(echo $WACOM_DEVICE | sed 's/.*id: //' | cut -d " " -f 1)"
   xsetwacom --set $WACOM_DEVICE_ID MapToOutput $MONITOR_NAME
-  echo "Mapped $WACOM_DEVICE to $MONITOR_NAME"
+  echo "Mapping $WACOM_DEVICE to $MONITOR_NAME..."
 done
 
 echo "Finished mapping devices."
